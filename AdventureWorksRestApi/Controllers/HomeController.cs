9802cb3198929af77d5ad664bb065603548fb2cb
Yauheni Masarnouski <Yauheni_Masarnouski@epam.com>
2018-11-20 15:37:27 +0300

    add home view

--- AdventureWorksRestApi/Controllers/HomeController.cs
+++ AdventureWorksRestApi/Controllers/HomeController.cs
@@ -1,100 +1,27 @@
-﻿using AdventureWorksRestApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
-using System.Net.Http;
-using System.Data.Entity;
-using Serilog;
+﻿using System.Web.Mvc;
 
 namespace AdventureWorksRestApi.Controllers
 {
-    [RoutePrefix("api")]
-    public class HomeController : ApiController
+    public class HomeController : Controller
     {
-        AdventureWorks2017Entities _context = new AdventureWorks2017Entities();
-
-        public HomeController()
+        public ActionResult Index()
         {
-            Log.Logger = new LoggerConfiguration().WriteTo.File(@"D:\Azure Training\Task2\AdventureWorksRestApi\AdventureWorksRestApi\log.txt",
-            outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}").CreateLogger();
-        }
-        [HttpGet]
-        [Route("products")]
-        public IHttpActionResult Read()
-        {
-            try
-            {
-                var products = _context.Products.Select(product => product);
-                Log.Information("Products successfully read");
-                return Ok(products);
-            }
-            catch 
-            {
-                Log.Information("There are some errors in the read method"); 
-                throw;
-            }
-        }
-        [HttpPost]
-        [Route("createProduct")]
-        public IHttpActionResult Create(Product product)
-        {
-            try
-            {
-                var newProduct = _context.Products.Add(product);
-                _context.SaveChanges();
-                Log.Information("New product successfully created");
-                return Ok(newProduct);
-            }
-            catch
-            {
-                Log.Information("Smth wrong in the create method");
-                throw;
-            }
-
+            return View();
         }
 
-        [HttpPut]
-        [Route("updateProduct")]
-        public IHttpActionResult Update(Product product)
+        public ActionResult About()
         {
-            try
-            {
-                _context.Entry(product).State = EntityState.Modified;
-                _context.SaveChanges();
-                Log.Information("New product successfully updated");
-                return Ok();
-            }
-            catch
-            {
-                Log.Information("Smth wrong in the update method");
-                throw;
-            }
+            ViewBag.Message = "Your application description page.";
+
+            return View();
         }
 
-        [HttpDelete]
-        [Route("deleteProduct/{productId}")]
-        public IHttpActionResult Delete(int productId)
+        public ActionResult Contact()
         {
-            try
-            {
-                var product = _context.Products.SingleOrDefault(pr => pr.ProductID == productId);
-                if (product == null)
-                {
-                    return this.NotFound();
-                }
-                _context.Products.Remove(product);
-                _context.SaveChanges();
-                Log.Information("New product successfully deleted");
-                return Ok();
-            }
-            catch
-            {
-                Log.Information("Smth wrong in the delete method");
-                throw;
-            }
-        }
+            ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
     }
 
 
---
+++ AdventureWorksRestApi/Controllers/SwaggerController.cs
@@ -0,0 +1,100 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+using AdventureWorksRestApi.Models;
+using Swashbuckle.Swagger.Annotations;
+using Serilog;
+
+namespace AdventureWorksRestApi.Controllers
+{
+    [RoutePrefix("api")]
+    public class SwaggerController : ApiController
+    {
+        AdventureWorks2017Entities _context = new AdventureWorks2017Entities();
+        
+        public SwaggerController()
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.File(
+                    @"D:\Azure Training\Task2\AdventureWorksRestApi\AdventureWorksRestApi\log.txt",
+                    outputTemplate:
+                    "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .CreateLogger();
+        }
+        [HttpGet]
+        [Route("products")]
+        public IHttpActionResult Read()
+        {
+            try
+            {
+                var products = _context.Products.Select(product => product);
+                Log.Information("Products successfully read");
+                return Ok(products);
+            }
+            catch
+            {
+                Log.Information("There are some errors in the read method");
+                throw;
+            }
+        }
+        [HttpPost]
+        [Route("createProduct")]
+        public IHttpActionResult Create(Product product)
+        {
+            try
+            {
+                var newProduct = _context.Products.Add(product);
+                _context.SaveChanges();
+                Log.Information("New product successfully created");
+                return Ok(newProduct);
+            }
+            catch
+            {
+                Log.Information("Smth wrong in the create method");
+                throw;
+            }
+
+        }
+
+        [HttpPut]
+        [Route("updateProduct")]
+        public IHttpActionResult Update(Product product)
+        {
+            try
+            {
+                _context.Entry(product).State = EntityState.Modified;
+                _context.SaveChanges();
+                Log.Information("New product successfully updated");
+                return Ok();
+            }
+            catch
+            {
+                Log.Information("Smth wrong in the update method");
+                throw;
+            }
+        }
+
+        [HttpDelete]
+        [Route("deleteProduct/{productId}")]
+        public IHttpActionResult Delete(int productId)
+        {
+            try
+            {
+                var product = _context.Products.SingleOrDefault(pr => pr.ProductID == productId);
+                if (product == null)
+                {
+                    return this.NotFound();
+                }
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+                Log.Information("New product successfully deleted");
+                return Ok();
+            }
+            catch
+            {
+                Log.Information("Smth wrong in the delete method");
+                throw;
+            }
+        }
+
+    }
+}
